03f00903bee7641abacfb7d65095a146bfb21c93
vantelindome <ant7dragons@yahoo.ne.jp>
2022-05-13 17:18:15 +0900

    added test directory

---
+++ test/test.py
@@ -0,0 +1,14 @@
+import math
+import numpy as np
+import random
+
+# for n in range(1000):
+#     a=np.random.uniform(0,2)
+#     b=np.random.uniform(0,5)
+#     c=random.uniform(0,2)
+#     d=random.uniform(0,5)
+#     print(a,b,c,d)
+
+print(math.pi)
+pie=math.pi 
+print(pie)
