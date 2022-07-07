9e692b4dc9c6a3a0df840c545163309c7e0f839f
vantelindome <ant7dragons@yahoo.ne.jp>
2022-07-07 20:47:44 +0900

    added r

--- final_report/problem_1/langevin.py
+++ final_report/problem_1/langevin.py
@@ -1,33 +1,33 @@
 from dataclasses import dataclass
 import numpy as np
+import math
 
 
 t=0.0
-dt=0.010
-m=0.1
-x=0.0 ; x1=0.0
-y=0.0 ; y1=0.0
-vx=0.0 ; vx1=0.0
-vy=0.0 ; vy1=0.0
+dt=0.01
+m=10.0 # m^*
+x=0.00 ; x1=0.00
+y=0.00 ; y1=0.00
+vx=0.00 ; vx1=0.00
+vy=0.00 ; vy1=0.00
+r=0.00 # <Δr^2>
 
-print(t,x,y)
 t = t + dt
 
-for t in range(10001):
-    Rg=np.random.rand()
+for n in range(10000):
+    Rg=np.random.randn()
     vx1 = (1-(dt/m))*vx + np.sqrt(2*dt)*Rg/m
     vy1 = (1-(dt/m))*vy + np.sqrt(2*dt)*Rg/m
 
     x1 = x + vx1*dt
     y1 = y + vy1*dt
+    r = 4 * m * (t/m + math.exp(-t/m) -1)
 
-    if t%10 == 0:
-        print(t,x1,y1)
-    
+    if n%10 == 0:
+        print(t,x1,y1,r)
     vx = vx1
     vy = vy1
     t = t + dt
-
 
 
 
