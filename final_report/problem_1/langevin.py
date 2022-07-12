26f7dec0eafb12ca462ee1b3a983fda6a303e9bf
vantelindome <ant7dragons@yahoo.ne.jp>
2022-07-12 19:34:43 +0900

    some changed about 1-4

--- final_report/problem_1/langevin.py
+++ final_report/problem_1/langevin.py
@@ -10,23 +10,24 @@ x=0.00 ; x1=0.00
 y=0.00 ; y1=0.00
 vx=0.00 ; vx1=0.00
 vy=0.00 ; vy1=0.00
-r=0.00 # <Δr^2>
 
 t = t + dt
 
 for n in range(10000):
-    Rg=np.random.randn()
-    vx1 = (1-(dt/m))*vx + np.sqrt(2*dt)*Rg/m
-    vy1 = (1-(dt/m))*vy + np.sqrt(2*dt)*Rg/m
+    Rg1=np.random.randn()
+    Rg2=np.random.randn()
+    vx1 = (1-(dt/m))*vx + np.sqrt(2*dt)*Rg1/m
+    vy1 = (1-(dt/m))*vy + np.sqrt(2*dt)*Rg2/m
 
     x1 = x + vx1*dt
     y1 = y + vy1*dt
-    r = 4 * m * (t/m + math.exp(-t/m) -1)
 
     if n%10 == 0:
-        print(t,x1,y1,r)
+        print(t,x1,y1)
     vx = vx1
     vy = vy1
+    x = x1
+    y = y1
     t = t + dt
 
 
